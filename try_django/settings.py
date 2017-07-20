02dc2492347a547fd433cb4ab5ad6f03751b3762
haessal <haessal@mizutamauki.net>
2017-07-20 14:55:44 +0000

    connect to mysql database
    In addtion, setup mysql and install python3 module as follows on Cloud9
     $ mysql-ctl start
     $ sudo pip3 install PyMySQL

--- try_django/settings.py
+++ try_django/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'try_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'c9',
+        'USER': 'haessal',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
