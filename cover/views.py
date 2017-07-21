bed22a541d31f01057abd5812333871ace277933
haessal <haessal@mizutamauki.net>
2017-07-21 11:54:12 +0000

    change to request login

--- cover/views.py
+++ cover/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
+@login_required
 def hello(request):
     context = {'user':'hoge'}
     return render(request, 'hello.html', context)
--- try_django/urls.py
+++ try_django/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 import cover.views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', cover.views.hello),
+    url(r'^accounts/login/$', auth_views.login, {'template_name':'login.html'}, name = 'login'),
 ]
